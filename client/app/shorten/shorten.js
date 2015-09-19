e13215020428eda55dddcc5fe39520945f1eb98e
youngfreezy <Fareez.Ahmed@gmail.com>
2015-09-18 20:49:47 -0700

    implement post requests for link shortener

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,11 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = '';
+
+  $scope.addLink = function (url) {
+    //something
+    Links.addLink(url);
+    //something else
+  }
 });
