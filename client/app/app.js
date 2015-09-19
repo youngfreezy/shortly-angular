a318b1b1812b68d54e57386d3d672ad4afa92579
youngfreezy <Fareez.Ahmed@gmail.com>
2015-09-18 17:11:33 -0700

    implement getLinks function and assigned data to data object in LinkController

--- client/app/app.js
+++ client/app/app.js
@@ -16,7 +16,7 @@ angular.module('shortly', [
       controller: 'AuthController'
     });
     // Your code here
-
+    
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
     $httpProvider.interceptors.push('AttachTokens');
--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,16 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  //scope object should have a data property 
+  $scope.data = {};
+  // scope object should have a .getLinks method
+  $scope.getLinks = function(){
+    // call the getLinks method from Links factory
+    Links.getLinks().then(function(data){
+      // assing the returned links to data
+      $scope.data.links = data.data;
+    });
+  };
+
+  $scope.getLinks();
 });
